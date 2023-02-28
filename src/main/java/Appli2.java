b1b6e8ae02468cd4cab9a3cd7d651a94edd1670d
marouane <marouaneaitsalah24@gmail.com>
2023-02-28 01:01:23 +0100

    Good

--- src/main/java/Appli2.java
+++ src/main/java/Appli2.java
@@ -1,7 +1,11 @@
 import Business.BankAccountService;
 import Business.BankAccountServiceImpl;
+import model.BankAccount;
 import model.CurrentAccount;
 import model.SavingAccount;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Appli2
 {
@@ -16,6 +20,23 @@ public class Appli2
         accountService.addBankAccount(new CurrentAccount("XE160940","MAD",46000,5000));
         accountService.addBankAccount(new SavingAccount("AB160941","MAD",45000,2.5));
 
-        System.out.println(accountService.getAccountbyId("MD544434"));
+        List<BankAccount>bankAccounts=accountService.getAllAccount();
+/*
+        for (int i = 0; i <bankAccounts.size() ; i++)
+        {
+            System.out.println(bankAccounts.get(i).toString());
+        }
+        */
+       /*
+       for(BankAccount bankAccount:bankAccounts){
+           System.out.println(bankAccount);
+       }
+*/
+       /*
+       bankAccounts.forEach(account -> {
+           System.out.println(account);
+       });
+       */
+
     }
 }
